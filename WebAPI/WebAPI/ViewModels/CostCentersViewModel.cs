d6395fc72aa69dcd58dc58165480a7645ad87e29
shivanjali <shivanjali.mahadik@satincorp.com>
2018-11-28 11:28:14 +0530

    CostCenter added

---
+++ WebAPI/WebAPI/ViewModels/CostCentersViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.ViewModels
+{
+    public class CostCentersViewModel
+    {
+        [Display(Name = "Cost Center name")]
+        [Required(ErrorMessage = "Cost Center name is required.")]
+        public string CostCenterName { get; set; }
+
+        public Guid Id { get; set; }
+
+        public IList<CostCenter> costList { get; set; }
+
+        public Guid? OrganizationUnitID { get; set; }
+    }
+}
